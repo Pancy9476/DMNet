e0eac99ececf7f40856f55ac8d22e9bfeaf3fabf
Pancy9476 <144259345+Pancy9476@users.noreply.github.com>
2025-06-13 20:27:11 +0800

    Add files via upload

---
+++ DMNet/models/sr3_modules/se.py
@@ -0,0 +1,70 @@
+
+
+from enum import Enum
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ChannelSELayer(nn.Module):
+    def __init__(self, num_channels, reduction_ratio=2):
+        super(ChannelSELayer, self).__init__()
+        num_channels_reduced = num_channels // reduction_ratio
+        self.reduction_ratio = reduction_ratio
+        self.fc1 = nn.Linear(num_channels, num_channels_reduced, bias=True)
+        self.fc2 = nn.Linear(num_channels_reduced, num_channels, bias=True)
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, input_tensor):
+        batch_size, num_channels, H, W = input_tensor.size()
+        squeeze_tensor = input_tensor.view(batch_size, num_channels, -1).mean(dim=2)
+
+        fc_out_1 = self.relu(self.fc1(squeeze_tensor))
+        fc_out_2 = self.sigmoid(self.fc2(fc_out_1))
+
+        a, b = squeeze_tensor.size()
+        output_tensor = torch.mul(input_tensor, fc_out_2.view(a, b, 1, 1))
+        return output_tensor
+
+
+class SpatialSELayer(nn.Module):
+
+    def __init__(self, num_channels):
+        super(SpatialSELayer, self).__init__()
+        self.conv = nn.Conv2d(num_channels, 1, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, input_tensor, weights=None):
+        batch_size, channel, a, b = input_tensor.size()
+
+        if weights is not None:
+            weights = torch.mean(weights, dim=0)
+            weights = weights.view(1, channel, 1, 1)
+            out = F.conv2d(input_tensor, weights)
+        else:
+            out = self.conv(input_tensor)
+        squeeze_tensor = self.sigmoid(out)
+        squeeze_tensor = squeeze_tensor.view(batch_size, 1, a, b)
+        output_tensor = torch.mul(input_tensor, squeeze_tensor)
+        return output_tensor
+
+
+class ChannelSpatialSELayer(nn.Module):
+
+    def __init__(self, num_channels, reduction_ratio=2):
+        super(ChannelSpatialSELayer, self).__init__()
+        self.cSE = ChannelSELayer(num_channels, reduction_ratio)
+        self.sSE = SpatialSELayer(num_channels)
+
+    def forward(self, input_tensor):
+        output_tensor = self.cSE(input_tensor) + self.sSE(input_tensor)
+        return output_tensor
+
+
+class SELayer(Enum):
+    NONE = 'NONE'
+    CSE = 'CSE'
+    SSE = 'SSE'
+    CSSE = 'CSSE'
